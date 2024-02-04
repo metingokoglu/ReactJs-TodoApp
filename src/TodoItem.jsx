0c34cefbbe7475dd4170d272bd67b71d52bdf85e
metingokoglu <metingkgl35@gmail.com>
2024-02-04 20:51:56 +0300

    TodoApp

---
+++ src/TodoItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodoItem = ({completed,id,title,toggleTodo,deleteTodo}) => {
+  return (
+    <li>
+        <label>
+         <input type="checkbox" checked={completed} onChange={e => toggleTodo(id,e.target.checked)}/>
+          {title}
+        </label>
+        <button onClick={()=>deleteTodo(id)} className="btn btn-danger">Delete</button>
+    </li>
+    )
+};
+
+export default TodoItem;
